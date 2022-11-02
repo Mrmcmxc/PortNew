62c34c85cc0f582cdc1c26d8fee36360ad4817d3
Hughesy90 <85328252+Hughesy90@users.noreply.github.com>
2022-11-02 14:28:44 +1300

    first commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# Getting Started with Create React App
+
+This project was bootstrapped with [Create React App]
+
+## Available Scripts
+
+In the project directory, you can run using :
+
+### `npm start`
+
+
+Builds the app for production to the `build` folder.\
+It correctly bundles React in production mode and optimizes the build for the best performance.
+
+The build is minified and the filenames include the hashes.\
+Your app is ready to be deployed!
+
---
+++ src/App.js
@@ -0,0 +1,27 @@
+import Navbar from "./components/Navbar";
+import Home from "./components/Home"
+import About from "./components/About"
+import Skills from "./components/Skills"
+import Work from "./components/Work"
+
+import Footer from "./components/Footer"
+import Contact from "./components/Contact";
+
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <Home />
+      <About />
+      <Skills />
+      <Work />
+      <Contact/>
+      <Footer />
+
+
+    </div>
+  );
+}
+
+export default App;
